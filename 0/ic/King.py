77883b8a6644e56984623dad24dd65a14534c24b
Olaf Willocx <olaf@usa.com>
2022-06-30 03:09:55 +0200

    _

--- 0/ic/King.py
+++ 0/ic/King.py
@@ -1,24 +1,28 @@
 import sys
 import numpy as np
-from scipy import special
-from scipy import integrate
-from scipy import interpolate
+from scipy import special, integrate, interpolate
 import h5py
 
-generator = np.random.default_rng(0)
+# TODO not confident in all my math being correct, rederive everything and
+#      verify
+# TODO make the Monte-Carlo sampling batched
 
 filename = sys.argv[1]
-n = int(sys.argv[2]) # objects
-N = int(sys.argv[3]) # interpolation resolution
+n        = int(sys.argv[2]) # objects
+N        = int(sys.argv[3]) # interpolation resolution
 # King (1966) parameters
-k = float(sys.argv[4])
-j = float(sys.argv[5])
+k  = float(sys.argv[4])
+j  = float(sys.argv[5])
 V0 = float(sys.argv[6])
 
 # TODO this explicit r_max and threshold_steps_to_boundary stuff is bs, do it
-#      automatically.
+#      automatically, somehow.
 r_max = 10 # should be slightly above r0
 threshold_steps_to_boundary = 128 # min steps to boundary, guarantees accuracy
+
+r_v     = np.empty([2, n])
+pos_vel = np.empty([2, n, 3])
+rng     = np.random.default_rng(0)
 
 def rho(V):
     return 0 if V>0 else np.sqrt(np.pi**3)*k/j**3*np.exp(2*j**2*(V0-V))*\
@@ -38,60 +42,53 @@ sol = integrate.solve_ivp(rhs, [0,r_max], [V0,0],
 V_interp = interpolate.interp1d(sol.t, sol.y[0], assume_sorted=True)
 
 # check if range is large enough
+# TODO use np.where, what was I thinking
 i = 0
 while sol.y[0,i] < 0:
     i += 1
     if i == N:
         raise RuntimeError(f"can't continue since r_max < r0")
-    print(f'{i} of {N} steps to boundary')
+print(f'{i} of {N} steps to boundary')
 if i < threshold_steps_to_boundary:
     raise RuntimeWarning('steps to boundary too small; reduce r_max and/or '
         'increase N')
 
 # calculate boundary
-# TODO there might be a more elegant way to do this with the interpolator
+# TODO there might be a more elegant way to do this with the interpolator, need
+#      to read documentation
 r0 = sol.t[i-1]-sol.y[0,i-1]*r_max/N/(sol.y[0,i]-sol.y[0,i-1])
 print(f'boundary at {r0:.2f} of {r_max:.2f}')
 
 # standard rejection sampling
-samples_r = np.empty(n)
-samples_v = np.empty(n)
 i = 0
 attempts = 0
 while i < n:
-    r = generator.uniform(0, r0)
-    v = generator.uniform(0, np.sqrt(-2*V_interp(r)))
-    # v = generator.uniform(0, np.sqrt(-2*V_interp(r)))
-    p = generator.uniform(0, 1/np.e**2)
+    r = rng.uniform(0, r0)
+    # TODO v is wrong here, need to use analytical max v
+    v = rng.uniform(0, np.sqrt(-2*V_interp(r)))
+    p = rng.uniform(0, 1/np.e**2)
     if p < r**2*v**2*np.exp(-2*j**2*(V_interp(r)-V0))*\
            (np.exp(-j**2*v**2)-np.exp(j**2*2*V_interp(r))):
-        samples_r[i] = r
-        samples_v[i] = v
+        r_v[:,i] = r, v
         i += 1
     attempts += 1
 print(f'{n/attempts*100:.2f}% sampling efficiency')
 
-# pos and vel isentropic
-# TODO I use pos and vel naming scheme, not r and v
-samples_r_theta = np.arccos(generator.uniform(-1, 1, n))
-samples_v_theta = np.arccos(generator.uniform(-1, 1, n))
-samples_r_phi = generator.uniform(0, 2*np.pi, n)
-samples_v_phi = generator.uniform(0, 2*np.pi, n)
+# random spatially isentropic spherical angles
+r_theta = np.arccos(rng.uniform(-1, 1, n))
+v_theta = np.arccos(rng.uniform(-1, 1, n))
+r_phi   = rng.uniform(0, 2*np.pi, n)
+v_phi   = rng.uniform(0, 2*np.pi, n)
 
 def spherical_to_Cartesian(r, theta, phi):
     return np.array([r*np.sin(theta)*np.cos(phi),
                      r*np.sin(theta)*np.sin(phi),
                      r*np.cos(theta)])
 
-positions  = spherical_to_Cartesian(samples_r, samples_r_theta, samples_r_phi)
-velocities = spherical_to_Cartesian(samples_v, samples_v_theta, samples_v_phi)
+pos_vel[0] = np.transpose(spherical_to_Cartesian(r_v[0], r_theta, r_phi))
+pos_vel[1] = np.transpose(spherical_to_Cartesian(r_v[1], v_theta, v_phi))
 
-# TODO ascontinguousarray is a bullshit fix for VTK trash, not longer necessary
-positions  = np.transpose(positions)
-velocities = np.transpose(velocities)
-
-# TODO store data
+# storage
 fp = h5py.File(filename, 'w')
-pos_vel = np.concatenate((positions, velocities)).reshape(2, n, 3)
 fp.create_dataset('pos,vel', data=pos_vel)
 fp.close()
--- 0/ic/Plummer.py
+++ 0/ic/Plummer.py
@@ -10,15 +10,17 @@ r_max_rel = float(sys.argv[4]) # distance limit relative to Plummer radius
 
 n_batch = int(np.sqrt(n))
 r_max   = a*r_max_rel
+
 r_v     = np.empty([2, n])
 pos_vel = np.empty([2, n, 3])
+rng     = np.random.default_rng(0)
 
-# rejection sampling
+# standard rejection sampling
 i = 0
 while i < n:
-    r = np.random.uniform(0, r_max, n_batch)
-    v = np.random.uniform(0, np.sqrt(2*n/np.sqrt(r_max**2+a**2)), n_batch)
-    p = np.random.uniform(0, 24*np.sqrt(2)/(7*np.pi**3)/(n*a)**1.5, n_batch)
+    r = rng.uniform(0, r_max, n_batch)
+    v = rng.uniform(0, np.sqrt(2*n/np.sqrt(r_max**2+a**2)), n_batch)
+    p = rng.uniform(0, 24*np.sqrt(2)/(7*np.pi**3)/(n*a)**1.5, n_batch)
     E = .5*v**2-n/np.sqrt(r**2+a**2)
     for j in range(n_batch):
         if i < n and E[j] < 0:
@@ -27,10 +29,10 @@ while i < n:
                 i += 1
 
 # random spatially isentropic spherical angles
-r_theta = np.arccos(np.random.uniform(-1, 1, n))
-v_theta = np.arccos(np.random.uniform(-1, 1, n))
-r_phi = np.random.uniform(0, 2*np.pi, n)
-v_phi = np.random.uniform(0, 2*np.pi, n)
+r_theta = np.arccos(rng.uniform(-1, 1, n))
+v_theta = np.arccos(rng.uniform(-1, 1, n))
+r_phi   = rng.uniform(0, 2*np.pi, n)
+v_phi   = rng.uniform(0, 2*np.pi, n)
 
 def spherical_to_Cartesian(r, theta, phi):
     return np.array([r*np.sin(theta)*np.cos(phi),
