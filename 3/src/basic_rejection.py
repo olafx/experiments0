df95a2597173b1871183f474fca488cbed1ab4fd
Olaf Willocx <olaf@usa.com>
2022-08-17 17:27:19 +0200

    rename

--- 3/src/basic_rejection.py
+++ 3/src/basic_rejection.py
@@ -1,13 +1,13 @@
 # Basic rejection sampling of a Student-t distribution. By basic is ment that
 # the secondary sampled distribution is a uniform distribution, between 0 and
 # the maximum of the distribution we're interested in.
-# A Student-t distribution is chosen for the example because it's fairly
+# A Student-t distribution is chosen for the example because it's relatively
 # difficult to sample from otherwise. The CDF requires the _2F_1 hypergeometric
 # function, with no obvious inverse. (A Gaussian also has a CDF with no obvious
 # inverse, but at least erf is common enough to work with.)
 # A Student-t distribution is also chosen because it highlights many of the
-# shortcomings of basic rejection sampling, and rejection sampling as a whole;
-# more on this later.
+# shortcomings of basic rejection sampling, and rejection sampling as a whole.
+# More on this later.
 
 import numpy as np
 from scipy import special
@@ -19,53 +19,51 @@ N    = 1000  # function plot resolution
 
 ups = 1 # DOF (Student-t parameter)
 
-# A Student-t distribution has long 'wings' as they are called; it does not tend
-# to a negligible probability very quickly. Because of this, a wide sampling
-# range is necessary to include the wings, and the value of the PDF is much
-# smaller than its maximum for most of the range. As a result, the sampling
-# efficiency will be poor; more on this later.
-t_range = [-20, 20]
+# A Student-t distribution has long 'wings', it does not tend to a negligible
+# probability very quickly. Because of this, a wide sampling range is necessary
+# to include the wings, and the value of the PDF is much smaller than its
+# maximum for most of the range. As a result, the sampling efficiency will be
+# poor. More on this later.
+x_range = [-20, 20]
 
 # Knowing the maximum of a distribution is necessary for rejection sampling.
-# This is a major issue of rejection sampling, because for real problems this is
-# often very difficult or impossible to determine. Sometimes it's also much
-# larger than the average of the PDF across the considered range. In such a case
-# the sampling efficiency will be poor; more on this later.
+# This is a major disadvanage of rejection sampling, because for real problems
+# this is often very difficult or impossible to determine. Sometimes it's also
+# much larger than the average of the PDF across the considered range. In such a
+# case the sampling efficiency will be poor. More on this later.
 f_max = special.gamma(.5*(ups+1))/np.sqrt(ups*np.pi)/special.gamma(.5*ups)
-# Student-t distribution
-def f(t):
-    return f_max*(1+t**2/ups)**(-.5*(ups+1))
+# Student-t distribution.
+def f(x):
+    return f_max*(1+x**2/ups)**(-.5*(ups+1))
 
 # Even with the wide range, everything farther and lower than the following
 # cutoff probability is lost. Notice that this is not that small; if millions of
 # samples are made, the samples are qualitatively highly improbable to have
 # come from the Student-t distribution, i.e. they can be considered poor quality
 # samples.
-print(f'probability cutoff {100*f(t_range[1]):.3f}%')
+print(f'probability cutoff {100*f(x_range[1]):.3f}%')
 
-# basic rejection sampling
+# Basic rejection sampling starts here.
 s = np.empty(n) # samples
 # A fast and high quality random number generator is of course key to any
-# Monte-Carlo method. Not discussed here. Using a random seed here to show how
-# the random nature of Monte-Carlo sampling; more on this later.
+# Monte-Carlo method. Not discussed here. Using a runtime random seed here to
+# show how the random nature of Monte-Carlo sampling. More on this later.
 rng = np.random.default_rng()
 i = 0 # sample count
 j = 0 # attempt count
 while i < n:
-    t = rng.uniform(t_range[0], t_range[1])
-    p = rng.uniform(0, f_max)
-    if p < f(t):
-        s[i] = t
+    x = rng.uniform(x_range[0], x_range[1])
+    if rng.uniform(0, f_max) < f(x):
+        s[i] = x
         i += 1
     j += 1
 
 # Sampling efficiency was mentioned earlier without definition. This is exactly
 # what is ment: the probability a sample will get accepted. This should ideally
 # be 1.
-# (Technically the sample count over attempt count is only a quantity that
-# approximates the true sampling efficiency; the sampling efficiency is not
-# random, but whatever. The obvious experimental sampling efficiency is given as
-# well.)
+# (The sample count over attempt count is a quantity that approximates the true
+# sampling efficiency; the sampling efficiency is not random. This obvious
+# experimental sampling efficiency estimator is given as well.)
 # The sampling efficiency for basic rejection sampling is trivial and need not
 # be explained, it just follows from the geometric interpretation of rejection
 # sampling.
@@ -73,22 +71,24 @@ while i < n:
 # disadvantage of rejection sampling, let alone an efficiency near 0, as is the
 # case here.
 # Notice how the true sampling efficiency is different from the experimental
-# one, and how the experimental sampling efficiency varies due to the new RNG
-# seed every time the program is ran.
+# one, and how the experimental sampling efficiency varies due to the new seed
+# every time the program is ran.
 # Notice how the true sampling efficiency tends to be slightly higher than the
 # experimental one. This is because it is assumed the integral of the PDF is 1
 # in the considered range. This isn't the case due to the finite range. The fact
 # that this is noticable again goes to show the range is not large enough.
-eff = 1/(f_max*(t_range[1]-t_range[0]))
-print(f'true         sampling efficiency {100*eff:.2f}%')
+eff = 1/(f_max*(x_range[1]-x_range[0]))
+print(f'true sampling efficiency {100*eff:.2f}%')
 print(f'experimental sampling efficiency {100*i/j:.2f}%')
 
-# plot
-t = np.linspace(t_range[0], t_range[1], N)
+# Plotting.
+x = np.linspace(x_range[0], x_range[1], N)
+plt.plot(x, [f(x_) for x_ in x], 'r', label='target Student-$t$')
+plt.plot(x_range, [f_max, f_max], 'b', label='proposal uniform')
 plt.hist(s, bins=bins, density=True, color='k')
-plt.plot(t, [f(t_) for t_ in t], 'r')
-plt.plot(t_range, [f_max, f_max], '--r')
-plt.xlim(t_range)
+plt.xlim(x_range)
+plt.xlabel('$x$')
 plt.ylabel('$p$')
+plt.legend()
 plt.tight_layout()
 plt.show()
--- 3/src/smart_rejection.py
+++ 3/src/smart_rejection.py
@@ -19,84 +19,83 @@ ups   = 1   # DOF (Student-t parameter)
 gamma = 1.2 # scale/HWHM (Lorentz parameter)
 alpha = 1.2 # scales the proposal distribution so that it's no longer a PDF
 
-# The sampling range can be orders of magnitudes larger here as in basic
+# The sampling range can be orders of magnitudes larger here than in basic
 # rejection sampling, because the efficiency will be much higher. If we have a
 # mathematical guarantee that the target distribution will always be larger than
 # the proposal distribution, it can even be made infinite.
 # The plot uses the same range as before for clarity.
-t_range      = [-1e3, 1e3]
-t_range_plot = [-20, 20]
+x_range      = [-1e3, 1e3]
+x_range_plot = [-20, 20]
 
-# Student-t distribution
+# Student-t distribution.
 f_max = special.gamma(.5*(ups+1))/np.sqrt(ups*np.pi)/special.gamma(.5*ups)
-def f_t(t):
-    return f_max*(1+t**2/ups)**(-.5*(ups+1))
+def f_t(x):
+    return f_max*(1+x**2/ups)**(-.5*(ups+1))
 
 # The distribution used to compare samples can't be normalized. If it was, it
 # would either have to be an identical distribution, i.e. useless because we
 # can't pull samples from it, or be smaller than the distribution we're
 # interested in in some areas, which is not allowed. If alpha were 1.1 here, it
 # would not work.
-# scaled Lorentzian distribution (not a PDF, unless alpha = 1)
+# Scaled Lorentzian distribution (not a PDF, unless alpha = 1).
 def f_L(x):
-    return alpha/(np.pi*gamma)*gamma**2/(gamma**2+x**2)
+    return alpha/np.pi*gamma/(gamma**2+x**2)
 
-# weak check to see if proposal > target everywhere
-for t in np.linspace(t_range[0], t_range[1], n):
-    if f_L(t) < f_t(t):
+# A weak check to see if proposal > target everywhere.
+for x in np.linspace(x_range[0], x_range[1], n):
+    if f_L(x) < f_t(x):
         raise RuntimeError('proposal distribution is smaller than target'
             'distribution somewhere')
 
 # The reason to choose a Lorentzian is its simple quantile function, along with
-# being very similar to the Student-t distribution. This will allow for high
-# sampling efficiency.
-# Lorentzian quantile function
+# being very similar to the Student-t distribution. This will allow for fast
+# and high efficiency sampling respectively.
+# Lorentzian quantile function.
 def Q_L(p):
     return gamma*np.tan(np.pi*(p-.5))
 
-# smart rejection sampling
+# Smart rejection sampling starts here.
 s = np.empty(n) # samples
-# Same random number generator, still random seed to show the randomness of
-# Monte-Carlo sampling.
+# Same random number generator, still with a runtime seed to show the randomness
+# of Monte-Carlo sampling.
 rng = np.random.default_rng()
-i = 0
-j = 0
+i = 0 # sample count
+j = 0 # attempt count
 while i < n:
-    t = Q_L(rng.uniform(0, 1))
-    if t < t_range[0] or t > t_range[1]:
+    x = Q_L(rng.uniform(0, 1))
+    if x < x_range[0] or x > x_range[1]:
         continue
-    p = rng.uniform(0, f_L(t))
-    if p < f_t(t):
-        s[i] = t
+    if rng.uniform(0, f_L(x)) < f_t(x):
+        s[i] = x
         i += 1
     j += 1
 
-# The true sampling efficiency if we considered an infinite range would be the
+# The true sampling efficiency if an infinite range is considered would be the
 # ratio of the integrals, which is trivially 1/alpha, since the target and
 # proposal distributions are PDFs, other than the alpha factor in front of the
 # proposal. We don't consider an infinite range, so it's not exactly correct.
 # That's why the experimental sampling efficiency might be slightly differnet on
 # average.
 # Notice how we went from <8% efficiency to >83% efficiency, and the parameters
-# alpha and gamma aren't even that optimized. The important conclusion is that
-# in this case, smart rejection sampling is fine, and more advanced Markov chain
-# Monte-Carlo methods are not necessary.
-print(f'true         sampling efficiency {100/alpha:.2f}%')
+# alpha and gamma aren't even that optimized. In this case smart rejection
+# sampling works well.
+print(f'true sampling efficiency {100/alpha:.2f}%')
 print(f'experimental sampling efficiency {100*i/j:.2f}%')
 
-# for the plot it's necessary to remove samples outside of the plot range,
-# because otherwise the bin width becomes too large
-s = s[~(s < t_range_plot[0])]
-s = s[~(s > t_range_plot[1])]
+# For the plot it's necessary to remove samples outside of the plot range,
+# because otherwise the bin width becomes too large.
+s = s[~(s < x_range_plot[0])]
+s = s[~(s > x_range_plot[1])]
 
-# plot
-t = np.linspace(t_range_plot[0], t_range_plot[1], 1000)
-plt.plot(t, [f_t(t_) for t_ in t], 'r', label='target Student-$t$')
-plt.plot(t_range, [f_max, f_max], '--r')
-plt.plot(t, [f_L(t_) for t_ in t], 'b', label='proposal Lorentzian')
+# Plotting.
+x = np.linspace(x_range_plot[0], x_range_plot[1], 1000)
+plt.plot(x, [f_t(x_) for x_ in x], 'r', label='target Student-$t$')
+plt.plot(x, [f_L(x_) for x_ in x], 'b', label='proposal Lorentzian')
 plt.hist(s, bins=bins, density=True, color='k')
-plt.xlim(t_range_plot)
+plt.xlim(x_range_plot)
+plt.xlabel('$x$')
 plt.ylabel('$p$')
 plt.legend()
 plt.tight_layout()
-plt.show()
+# plt.show()
+plt.savefig('2.png', dpi=400)
